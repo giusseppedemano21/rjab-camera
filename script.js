83f06e37fd04405b17a4dc4824b8555b59d36457
giusseppedemano21 <giusseppedemano21@gmail.com>
2026-08-03 07:52:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -658,7 +658,7 @@ const lineHeight = Math.round(19 * scale);
 
             const cardHeight = 200;
 
-            const cardX = 5;
+            const cardX = 3;
 
             const cardY =
                 canvas.height - cardHeight - 3;
